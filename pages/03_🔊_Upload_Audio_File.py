613b97eda02dd2e12e35d43854a5a9758c8c4061
BatuhanYilmaz26 <batuhanylmz26@outlook.com>
2022-10-25 20:25:30 +0300

    .

--- pages/03_🔊_Upload_Audio_File.py
+++ pages/03_🔊_Upload_Audio_File.py
@@ -1,18 +1,16 @@
 import whisper
-from pytube import YouTube
-import requests
-import time
 import streamlit as st
 from streamlit_lottie import st_lottie
-import numpy as np
-import os
-from typing import Iterator
-from io import StringIO
 from utils import write_vtt, write_srt
 import ffmpeg
-from languages import LANGUAGES
+import requests
+from typing import Iterator
+from io import StringIO
+import numpy as np
+import pathlib
+import os
 
-st.set_page_config(page_title="Auto Subtitled Video Generator", page_icon=":movie_camera:", layout="wide")
+st.set_page_config(page_title="Auto Transcriber", page_icon="🔊", layout="wide")
 
 # Define a function that we can use to load lottie files from a link.
 @st.cache(allow_output_mutation=True)
@@ -22,42 +20,27 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
+
+APP_DIR = pathlib.Path(__file__).parent.absolute()
+
+LOCAL_DIR = APP_DIR / "local_audio"
+LOCAL_DIR.mkdir(exist_ok=True)
+save_dir = LOCAL_DIR / "output"
+save_dir.mkdir(exist_ok=True)
+
+
 col1, col2 = st.columns([1, 3])
 with col1:
-    lottie = load_lottieurl("https://assets8.lottiefiles.com/packages/lf20_jh9gfdye.json")
+    lottie = load_lottieurl("https://assets1.lottiefiles.com/packages/lf20_1xbk4d2v.json")
     st_lottie(lottie, speed=1, height=250, width=250)
 
 with col2:
     st.write("""
-    ## Auto Subtitled Video Generator 
-    ##### Input a YouTube video link and get a video with subtitles.
-    ###### ➠ If you want to transcribe the video in its original language, select the task as "Transcribe"
-    ###### ➠ If you want to translate the subtitles to English, select the task as "Translate" 
+    ## Auto Transcriber
+    ##### Input an audio file and get a transcript.
+    ###### ➠ If you want to transcribe the audio in its original language, select the task as "Transcribe"
+    ###### ➠ If you want to translate the transcription to English, select the task as "Translate" 
     ###### I recommend starting with the base model and then experimenting with the larger models, the small and medium models often work well. """)
-    
-
-@st.cache(allow_output_mutation=True)
-def populate_metadata(link):
-    yt = YouTube(link)
-    author = yt.author
-    title = yt.title
-    description = yt.description
-    thumbnail = yt.thumbnail_url
-    length = yt.length
-    views = yt.views
-    return author, title, description, thumbnail, length, views
-
-
-@st.cache(allow_output_mutation=True)
-def download_video(link):
-    yt = YouTube(link)
-    video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-    return video
-
-
-def convert(seconds):
-    return time.strftime("%H:%M:%S", time.gmtime(seconds))
-
 
 loaded_model = whisper.load_model("base")
 current_size = "None"
@@ -71,21 +54,23 @@ def change_model(current_size, size):
     else:
         raise Exception("Model size is the same as the current size.")
 
-
 @st.cache(allow_output_mutation=True)
-def inference(link, loaded_model, task):
-    yt = YouTube(link)
-    path = yt.streams.filter(only_audio=True)[0].download(filename="audio.mp4")
+def inferecence(loaded_model, uploaded_file, task):
+    with open(f"{save_dir}/input.mp3", "wb") as f:
+            f.write(uploaded_file.read())
+    audio = ffmpeg.input(f"{save_dir}/input.mp3")
+    audio = ffmpeg.output(audio, f"{save_dir}/output.wav", acodec="pcm_s16le", ac=1, ar="16k")
+    ffmpeg.run(audio, overwrite_output=True)
     if task == "Transcribe":
         options = dict(task="transcribe", best_of=5)
-        results = loaded_model.transcribe(path, **options)
+        results = loaded_model.transcribe(f"{save_dir}/output.wav", **options)
         vtt = getSubs(results["segments"], "vtt", 80)
         srt = getSubs(results["segments"], "srt", 80)
         lang = results["language"]
         return results["text"], vtt, srt, lang
     elif task == "Translate":
         options = dict(task="translate", best_of=5)
-        results = loaded_model.transcribe(path, **options)
+        results = loaded_model.transcribe(f"{save_dir}/output.wav", **options)
         vtt = getSubs(results["segments"], "vtt", 80)
         srt = getSubs(results["segments"], "srt", 80)
         lang = results["language"]
@@ -94,7 +79,6 @@ def inference(link, loaded_model, task):
         raise ValueError("Task not supported")
 
 
-@st.cache(allow_output_mutation=True)
 def getSubs(segments: Iterator[dict], format: str, maxLineWidth: int) -> str:
     segmentStream = StringIO()
 
@@ -109,103 +93,27 @@ def getSubs(segments: Iterator[dict], format: str, maxLineWidth: int) -> str:
     return segmentStream.read()
 
 
-def get_language_code(language):
-    if language in LANGUAGES.keys():
-        detected_language = LANGUAGES[language]
-        return detected_language
-    else:
-        raise ValueError("Language not supported")
-
-
-def generate_subtitled_video(video, audio, transcript):
-    video_file = ffmpeg.input(video)
-    audio_file = ffmpeg.input(audio)
-    ffmpeg.concat(video_file.filter("subtitles", transcript), audio_file, v=1, a=1).output("final.mp4").run(quiet=True, overwrite_output=True)
-    video_with_subs = open("final.mp4", "rb")
-    return video_with_subs        
-    
-
 def main():
     size = st.selectbox("Select Model Size (The larger the model, the more accurate the transcription will be, but it will take longer)", ["tiny", "base", "small", "medium", "large"], index=1)
     loaded_model = change_model(current_size, size)
     st.write(f"Model is {'multilingual' if loaded_model.is_multilingual else 'English-only'} "
         f"and has {sum(np.prod(p.shape) for p in loaded_model.parameters()):,} parameters.")
-    link = st.text_input("YouTube Link (The longer the video, the longer the processing time)")
+    input_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "m4a"])
+    if input_file is not None:
+        filename = input_file.name[:-4]
+    else:
+        filename = None
     task = st.selectbox("Select Task", ["Transcribe", "Translate"], index=0)
     if task == "Transcribe":
         if st.button("Transcribe"):
-            author, title, description, thumbnail, length, views = populate_metadata(link)
-            results = inference(link, loaded_model, task)
-            video = download_video(link)
-            lang = results[3]
-            detected_language = get_language_code(lang)
-                
+            results = inferecence(loaded_model, input_file, task)
             col3, col4 = st.columns(2)
-            col5, col6, col7, col8 = st.columns(4)
+            col5, col6, col7 = st.columns(3)
             col9, col10 = st.columns(2)
-            with col3:
-                st.video(video)
-                
-            # Write the results to a .txt file and download it.
-            with open("transcript.txt", "w+", encoding='utf8') as f:
-                f.writelines(results[0])
-                f.close()
-            with open(os.path.join(os.getcwd(), "transcript.txt"), "rb") as f:
-                datatxt = f.read()
-                
-
-                with open("transcript.vtt", "w+",encoding='utf8') as f:
-                    f.writelines(results[1])
-                    f.close()
-                with open(os.path.join(os.getcwd(), "transcript.vtt"), "rb") as f:
-                    datavtt = f.read()
-                
-                with open("transcript.srt", "w+",encoding='utf8') as f:
-                    f.writelines(results[2])
-                    f.close()
-                with open(os.path.join(os.getcwd(), "transcript.srt"), "rb") as f:
-                    datasrt = f.read()
-            with col5:
-                st.download_button(label="Download Transcript (.txt)",
-                                data=datatxt,
-                                file_name="transcript.txt")
-            with col6:   
-                st.download_button(label="Download Transcript (.vtt)",
-                                    data=datavtt,
-                                    file_name="transcript.vtt")
-            with col7:
-                st.download_button(label="Download Transcript (.srt)",
-                                    data=datasrt,
-                                    file_name="transcript.srt")
-            with col9:
-                st.success("You can download the transcript in .srt format, edit it (if you need to) and upload it to YouTube to create subtitles for your video.")
-            with col10:
-                st.info("Streamlit refreshes after the download button is clicked. The data is cached so you can download the transcript again without having to transcribe the video again.")
             
-            with col4:
-                with st.spinner("Generating Subtitled Video"):
-                    video_with_subs = generate_subtitled_video(video, "audio.mp4", "transcript.srt")
-                st.video(video_with_subs)
-                st.balloons()
-            with col8:
-                st.download_button(label="Download Subtitled Video",
-                                    data=video_with_subs,
-                                    file_name=f"{title} with subtitles.mp4")
-    elif task == "Translate":
-        if st.button("Translate to English"):
-            author, title, description, thumbnail, length, views = populate_metadata(link)
-            results = inference(link, loaded_model, task)
-            video = download_video(link)
-            lang = results[3]
-            detected_language = get_language_code(lang)
-                
-            col3, col4 = st.columns(2)
-            col5, col6, col7, col8 = st.columns(4)
-            col9, col10 = st.columns(2)
             with col3:
-                st.video(video)
+                st.audio(input_file)
                 
-            # Write the results to a .txt file and download it.
             with open("transcript.txt", "w+", encoding='utf8') as f:
                 f.writelines(results[0])
                 f.close()
@@ -224,6 +132,7 @@ def main():
                 f.close()
             with open(os.path.join(os.getcwd(), "transcript.srt"), "rb") as f:
                 datasrt = f.read()
+
             with col5:
                 st.download_button(label="Download Transcript (.txt)",
                                 data=datatxt,
@@ -240,16 +149,53 @@ def main():
                 st.success("You can download the transcript in .srt format, edit it (if you need to) and upload it to YouTube to create subtitles for your video.")
             with col10:
                 st.info("Streamlit refreshes after the download button is clicked. The data is cached so you can download the transcript again without having to transcribe the video again.")
-            
-            with col4:
-                with st.spinner("Generating Subtitled Video"):
-                    video_with_subs = generate_subtitled_video(video, "audio.mp4", "transcript.srt")
-                st.video(video_with_subs)
-                st.balloons()
-            with col8:
-                st.download_button(label="Download Subtitled Video",
-                                    data=video_with_subs,
-                                    file_name=f"{title} with subtitles.mp4")
+
+    elif task == "Translate":
+        if st.button("Translate to English"):
+            results = inferecence(loaded_model, input_file, task)
+            col3, col4 = st.columns(2)
+            col5, col6, col7 = st.columns(3)
+            col9, col10 = st.columns(2)
+
+            with col3:
+                st.audio(input_file)
+                
+            with open("transcript.txt", "w+", encoding='utf8') as f:
+                f.writelines(results[0])
+                f.close()
+            with open(os.path.join(os.getcwd(), "transcript.txt"), "rb") as f:
+                datatxt = f.read()
+                
+
+            with open("transcript.vtt", "w+",encoding='utf8') as f:
+                f.writelines(results[1])
+                f.close()
+            with open(os.path.join(os.getcwd(), "transcript.vtt"), "rb") as f:
+                datavtt = f.read()
+                
+            with open("transcript.srt", "w+",encoding='utf8') as f:
+                f.writelines(results[2])
+                f.close()
+            with open(os.path.join(os.getcwd(), "transcript.srt"), "rb") as f:
+                datasrt = f.read()
+                
+            with col5:
+                st.download_button(label="Download Transcript (.txt)",
+                                data=datatxt,
+                                file_name="transcript.txt")
+            with col6:   
+                st.download_button(label="Download Transcript (.vtt)",
+                                    data=datavtt,
+                                    file_name="transcript.vtt")
+            with col7:
+                st.download_button(label="Download Transcript (.srt)",
+                                    data=datasrt,
+                                    file_name="transcript.srt")
+            with col9:
+                st.success("You can download the transcript in .srt format, edit it (if you need to) and upload it to YouTube to create subtitles for your video.")
+            with col10:
+                st.info("Streamlit refreshes after the download button is clicked. The data is cached so you can download the transcript again without having to transcribe the video again.")
+
     else:
         st.error("Please select a task.")
 
